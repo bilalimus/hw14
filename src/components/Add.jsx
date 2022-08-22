c35da7963f8ca959ecc1cf10025b8f1980de6f09
Maksat <bilalimus@gmail.com>
2022-08-22 23:55:22 +0600

    Create class components

---
+++ src/components/Add.jsx
@@ -0,0 +1,14 @@
+import { Component } from "react";
+
+class Add extends Component {
+  render() {
+    return (
+      <>
+        <input type="text" />
+        <button>add</button>
+      </>
+    );
+  }
+}
+
+export default Add;
